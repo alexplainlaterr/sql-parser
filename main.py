32830aa04c00a57de9c4126e158ad837145d17d2
Alex Wong <alex.wong@thescore.com>
2024-03-21 22:33:06 -0400

    Print tables and their frequency of occurrence

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from pathlib import Path
 from sql_metadata import Parser
 
 def process_sql_queries(df):
-    # Initialize an empty set to store all tables
-    all_tables = set()
+    # Initialize an empty list to store all tables
+    all_tables = []
 
     for index, row in df.iterrows():
         sql_query = row['sql_unload']
         try:
             tables = Parser(sql_query).tables
             if tables is not None:
-                all_tables.update(tables)
+                # Extend the list with tables from the current query that contain a period in the name
+                all_tables.extend(table for table in tables if '.' in table)
                 print(f"Query {index + 1}:")
                 print(tables)
                 print()
@@ -22,16 +23,17 @@ def process_sql_queries(df):
             print(f"Error parsing query {index + 1}: {e}")
             print()
 
-    # Filter out table names that don't contain a period
-    all_tables_with_period = {table for table in all_tables if '.' in table}
-
     # Convert the set of tables to a list and sort it alphabetically
-    all_tables_list = sorted(list(all_tables_with_period))
+    all_tables_list = sorted(list(all_tables))
 
-    # Print the distinct list of tables
-    print("Distinct list of tables:")
-    for table in all_tables_list:
-        print(table)
+    # Convert list to data frame
+    df = pd.DataFrame({'col': all_tables_list})
+
+    # Count table occurrence
+    grouped_df = df.groupby('col').size().reset_index(name='count')
+    sorted_df = grouped_df.sort_values(by='count', ascending=False)
+    print(sorted_df)
+
 
 # Real usage:
 directory = Path(r'/Users/Alex.Wong/Documents/GitHub/sql-parser')
